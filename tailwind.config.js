b5e4713727d2f490ce7642eb4bf07cb368eaf97b
Lucas Aguiar <luacas99.abreu@gmail.com>
2022-01-30 15:34:01 -0300

    setup ok

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+    },
+
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      flex: {
+        2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
+        5: "5 5 0%",
+        6: "6 6 0%",
+        7: "7 7 0%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
